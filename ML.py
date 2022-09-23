978a23986895bfe7fecc0c26534069a7eb178ffb
CamBurr <burr.cameron@gmail.com>
2022-09-23 02:32:21 -0600

    replaced user data collection with scraper.py, which is much more efficient.

--- ML.py
+++ ML.py
@@ -13,88 +13,17 @@ import time
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 
-def cache_dict(files):
-    for key in files:
-        with open(key + '.pickle', 'wb') as handle:
-            pickle.dump(files[key], handle)
-
-
 def dict_load():
-    variables = ['users', 'text', 'scores', 'labels']
-    to_return = dict()
-    for i in variables:
-        with open(i + ".pickle", 'rb') as handle:
-            to_return[i] = pickle.load(handle)
+    with open('glob.pickle', 'rb') as fp:
+        to_return = pickle.load(fp)
     return to_return
-
-
-def user_loop(session):
-    if exists('users.pickle'):
-        tensors = dict_load()
-        text_tensors = tensors['text']
-        score_tensors = tensors['scores']
-        users = tensors['users']
-        labels = tensors['labels']
-    else:
-        text_tensors = []
-        score_tensors = []
-        labels = []
-        users = set()
-
-    subreddit = session.subreddit('politicalcompassmemes')
-
-    new_users = set()
-    new_labels = []
-    new_text_tensors = []
-    new_score_tensors = []
-
-    for comment in subreddit.stream.comments():
-        if comment.author not in users and comment.author not in new_users:
-            if comment.author_flair_text is None:
-                continue
-            else:
-                if 'Right' in comment.author_flair_text:
-                    new_labels.append(1)
-                elif 'Left' in comment.author_flair_text:
-                    new_labels.append(-1)
-                elif any(flair in comment.author_flair_text for flair in ['Center', 'Centrist']):
-                    new_labels.append(0)
-
-            user = comment.author
-            text_tensor = []
-            score_tensor = []
-            for i in user.comments.new(limit=25):
-                text = ''.join(e for e in i.body if (e.isalnum() or e == ' '))
-                text_tensor.append(text)
-                score_tensor.append(i.score)
-            text_tensor += ' ' * (25 - len(text_tensor))
-            score_tensor.extend([0] * (25 - len(score_tensor)))
-            new_score_tensors.append(score_tensor)
-            new_text_tensors.append(text_tensor)
-            new_users.add(user)
-
-            if len(new_users) % 25 == 0:
-                print(len(users))
-                text_tensors.extend(new_text_tensors)
-                score_tensors.extend(new_score_tensors)
-                users.update(new_users)
-                labels.extend(new_labels)
-                new_users = set()
-                new_score_tensors = []
-                new_text_tensors = []
-                new_labels = []
-
-            cache_dict({'users': users, 'text': text_tensors, 'scores': score_tensors, 'labels': labels})
-
-        if len(users) >= 30000:
-            return {'users': users, 'text': text_tensors, 'scores': score_tensors, 'labels': labels}
 
 
 def preprocessing(tensors):
     time_a = time.time_ns()
     text_tensors = tensors['text']
     score_tensors = tensors['scores']
-    labels = np.asarray([x+1 for x in tensors['labels']])
+    labels = np.asarray(tensors['labels'])
     vocab_size = 2000
 
     vectorize_layer: TextVectorization = layers.TextVectorization(max_tokens=10000, output_mode='count')
@@ -112,35 +41,12 @@ def preprocessing(tensors):
         with open('vocab.pickle', 'wb') as handle:
             pickle.dump(vectorize_layer.get_vocabulary(), handle)
 
-    nested_text = text_tensors
-
-    for i in range(len(text_tensors)):
-        for j in range(len(text_tensors[0])):
-            nested_text[i][j] = [text_tensors[i][j]]
-
-    score_tensors = np.asarray(score_tensors)
-    # full_tensor = tf.convert_to_tensor(np.asarray(nested_text))
-    # print(full_tensor)
-    print(time.time_ns() - time_a)
-
-    flat_array = []
-
-    for sublist_1 in nested_text:
-        flat_sublist = []
-        for sublist_2 in sublist_1:
-            flat_sublist.append(sublist_2[0])
-        flat_array.append(' '.join(flat_sublist))
-
     sigendian = -vocab_size + vocab_size
 
-    text_tensors = np.asarray(tf.cast(vectorize_layer(flat_array), tf.float64))[19:, 1:]
+    text_tensors = np.asarray(tf.cast(vectorize_layer(text_tensors), tf.float64))[19:, 1:]
     text_tensors = text_tensors / text_tensors.sum(axis=1)[:, None] * 100
-    score_tensors = np.asarray(score_tensors)
-    print(score_tensors.mean(axis=1, keepdims=True))
-    text_tensors = np.append(text_tensors, score_tensors.mean(axis=1, keepdims=True)[19:, :], axis=1)[:, sigendian:]
-
-    print(text_tensors.shape)
-    print(score_tensors.mean(axis=1).shape)
+    score_tensors = np.asarray([[a] for a in score_tensors])[19:]
+    text_tensors = np.append(text_tensors, score_tensors, axis=1)[:, sigendian:]
 
     batch = 256
 
@@ -154,7 +60,7 @@ def preprocessing(tensors):
         layers.Dropout(.3),
         layers.Dense(100),
         layers.Dropout(.3),
-        layers.Dense(3, activation='softmax'),
+        layers.Dense(9, activation='softmax')
     ])
 
     reg_model = keras.Sequential([
@@ -175,7 +81,7 @@ def preprocessing(tensors):
     for label in labels:
         nested_labels.append([label])
 
-    class_model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.000005),
+    class_model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
                         loss=losses.SparseCategoricalCrossentropy(from_logits=False), metrics='accuracy')
     reg_model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
                       loss=losses.MeanSquaredError(), metrics='accuracy')
@@ -184,21 +90,7 @@ def preprocessing(tensors):
     print(np.append(class_model.predict(text_tensors[:19, sigendian:]), nested_labels[:19], axis=1))
     print(np.append(reg_model.predict(text_tensors[:19, sigendian:]), nested_labels[:19], axis=1))
 
-    '''class_pred = class_model.predict(text_tensors[19:, sigendian:])
-    reg_pred = reg_model.predict(text_tensors[19:, sigendian:])
-    combined_pred = np.append(class_pred, reg_pred, axis=1)
-    combined_val = np.append(class_model.predict(text_tensors[:19, sigendian:]),
-                             reg_model.predict(text_tensors[:19, sigendian:]), axis=1)
-    final_model = keras.Sequential([
-        layers.Dense(50),
-        layers.Dense(1)
-    ])
-    final_model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
-                        loss=losses.MeanSquaredError(), metrics='accuracy')
-    final_model.fit(x=combined_pred, y=labels[19:], epochs=100, batch_size=batch, validation_split=.05)
-    print(np.append(final_model.predict(combined_val[:, sigendian:]), nested_labels[:19], axis=1))'''
-
 
 if __name__ == '__main__':
-    reddit = praw.Reddit("DEFAULT")
-    preprocessing(user_loop(reddit))
+    glob = dict_load()
+    preprocessing(glob)
---
+++ scraper.py
@@ -0,0 +1,115 @@
+import requests
+import json
+import pickle
+import time
+import string
+
+data_file = "data.pickle"
+glob_file = "glob.pickle"
+size = 250
+
+
+try:
+    with open(data_file, 'rb') as fp:
+        dataSet = pickle.load(fp)
+        oldestTS = min(a['created_utc'] for a in dataSet.values())
+except FileNotFoundError as error:
+    oldestTS = int(time.time())
+    dataSet = dict()
+
+
+def fmt(flair):
+    if flair is not None:
+        flair = flair.split(':')[1].lower()
+        if flair[0] == 'c':
+            return 'centrist'
+        if flair[-1] == '2':
+            return 'libright'
+        return flair
+    else:
+        return None
+
+
+def get_subreddit_data(a):
+    url = 'https://api.pushshift.io/reddit/comment/search/?size='+str(size)+'&before='+str(a) + \
+          '&subreddit=PoliticalCompassMemes&filter=id,author,author_flair_text,created_utc'
+    r = requests.get(url)
+    if r:
+        data = json.loads(r.text)
+        return data['data']
+    else:
+        time.sleep(3)
+        return get_subreddit_data(a)
+
+
+def get_user_comments(author):
+    url = 'https://api.pushshift.io/reddit/comment/search/?size=50&author={}&filter=body,score'.format(author)
+    r = requests.get(url)
+    if r:
+        comments = json.loads(r.text)['data']
+        cleaned = []
+        for comment in comments:
+            if 'https://' not in comment['body'] and 'http://' not in comment['body']:
+                new_body = ''.join(filter(lambda x: x in string.printable and x not in ',./?";\'`:;!@#$%^&*()_+1234567890-=\\|}{[]',
+                                          comment['body'])).replace('\n', ' ')
+                cleaned.append({'body': new_body,
+                                'score': comment['score']})
+
+        flat = ' '.join([c['body'] for c in cleaned])
+
+        return cleaned, flat
+    else:
+        print(r)
+        time.sleep(4)
+        return get_user_comments(author)
+
+before = oldestTS
+data = get_subreddit_data(before)
+cache_flag = 0
+
+while len(data) >= 0 and len(dataSet) < 30000:
+    new_users = 0
+    for c in data:
+        if c['author'] not in dataSet:
+            dataSet[c['author']] = {'created_utc': c['created_utc'],
+                                    'id': c['id'],
+                                    'author_flair_text': fmt(c['author_flair_text'])}
+            new_users += 1
+
+    cache_flag += new_users
+    if cache_flag >= 1000:
+        with open(data_file, 'wb') as fp:
+            pickle.dump(dataSet, fp)
+        cache_flag = 0
+        print("Cached dataset.")
+
+    print('Got {total} users, added {new_users} users for a total of {full} users.'.format(total=len(data), new_users=new_users, full=len(dataSet)))
+    data = get_subreddit_data(data[-1]['created_utc'])
+
+score_tensor = []
+text_tensor = []
+labels = []
+
+flairs = ['left', 'right', 'lib', 'auth', 'centrist', 'authright', 'libright', 'libleft', 'authleft']
+cache_flag = 0
+
+for user in dataSet:
+    user_flair = dataSet[user]['author_flair_text']
+    if user_flair in flairs:
+        comments, flat = get_user_comments(user)
+        if len(comments) > 0:
+            text_tensor.append(flat)
+            score_tensor.append(round(sum(c['score'] for c in comments)/len(comments), 1))
+            labels.append(flairs.index(user_flair))
+            print(flairs.index(user_flair))
+        cache_flag += 1
+        if cache_flag >= 50:
+            glob = {'text': text_tensor, 'scores': score_tensor, 'labels': labels}
+            with open(glob_file, 'wb') as fp:
+                pickle.dump(glob, fp)
+            cache_flag = 0
+            print(len(score_tensor), len(text_tensor))
+
+glob = {'text': text_tensor, 'scores': score_tensor, 'labels': labels}
+with open(glob_file, 'wb') as fp:
+    pickle.dump(glob, fp)
