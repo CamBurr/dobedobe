bd5108e50bc60fc894efd9eda9068804711d075e
CamBurr <burr.cameron@gmail.com>
2022-09-23 12:06:41 -0600

    added compressed glob, changed ML for compatibility with data from new API

--- ML.py
+++ ML.py
@@ -1,6 +1,5 @@
 from os.path import exists
 import os
-import praw
 import pickle
 import tensorflow as tf
 from keras.layers import TextVectorization
@@ -23,11 +22,10 @@ def dict_load():
 
 
 def preprocessing(tensors):
-    time_a = time.time_ns()
     text_tensors = tensors['text']
     score_tensors = tensors['scores']
     labels = np.asarray(tensors['labels'])
-    vocab_size = 2000
+    vocab_size = 3000
 
     vectorize_layer: TextVectorization = layers.TextVectorization(max_tokens=10000, output_mode='count')
     flat_text = []
@@ -46,16 +44,17 @@ def preprocessing(tensors):
 
     sigendian = -vocab_size + vocab_size
 
-    text_tensors = np.asarray(tf.cast(vectorize_layer(text_tensors), tf.float64))[19:, 1:]
-    text_tensors = text_tensors / text_tensors.sum(axis=1)[:, None] * 100
+    text_tensors = np.asarray(tf.cast(vectorize_layer(text_tensors), tf.float64))[19:, :]
+    sum_tensor = np.asarray([i if i != 0 else 1 for i in text_tensors.sum(axis=1)])
+    text_tensors = text_tensors / sum_tensor[:, None] * 100
     score_tensors = np.asarray([[a] for a in score_tensors])[19:]
     text_tensors = np.append(text_tensors, score_tensors, axis=1)[:, sigendian:]
 
-    batch = 256
+    batch = 512
 
     class_model = keras.Sequential([
-        layers.Dropout(.1),
-        layers.Dense(2000, activation='relu'),
+        layers.Dropout(.3),
+        layers.Dense(2000, activation='relu', use_bias=False),
         layers.Dropout(.3),
         layers.Dense(1000),
         layers.Dropout(.3),
@@ -63,18 +62,7 @@ def preprocessing(tensors):
         layers.Dropout(.3),
         layers.Dense(100),
         layers.Dropout(.3),
-        layers.Dense(9, activation='softmax')
-    ])
-
-    reg_model = keras.Sequential([
-        layers.GaussianNoise(.1),
-        layers.Dropout(.1),
-        layers.Dense(1000, activation='relu', use_bias=False),
-        layers.Dropout(.1),
-        layers.Dense(100, use_bias=False),
-        layers.Dropout(.1),
-        layers.Dense(10, use_bias=False),
-        layers.Dense(1),
+        layers.Dense(9)
     ])
 
     np.set_printoptions(suppress=True)
@@ -84,14 +72,11 @@ def preprocessing(tensors):
     for label in labels:
         nested_labels.append([label])
 
-    class_model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
-                        loss=losses.SparseCategoricalCrossentropy(from_logits=False), metrics='accuracy')
-    reg_model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.0001),
-                      loss=losses.MeanSquaredError(), metrics='accuracy')
+    class_model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.00001),
+                        loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+                        metrics='sparse_categorical_accuracy')
     class_model.fit(x=text_tensors, y=labels[19:], epochs=40, batch_size=batch, validation_split=.05)
-    reg_model.fit(x=text_tensors, y=labels[19:], epochs=40, batch_size=batch, validation_split=.05)
     print(np.append(class_model.predict(text_tensors[:19, sigendian:]), nested_labels[:19], axis=1))
-    print(np.append(reg_model.predict(text_tensors[:19, sigendian:]), nested_labels[:19], axis=1))
 
 
 if __name__ == '__main__':
