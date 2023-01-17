58cc4dfce91550b4dbfdd0c77c12ead4e345909f
CamBurr <burr.cameron@gmail.com>
2023-01-17 14:25:49 -0700

    modified caching

--- ML.py
+++ ML.py
@@ -13,10 +13,10 @@ import random
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 cached_dict_file = "Data/cached_dict.pickle"
 cached_balanced_file = "Data/cached_balanced_dict.pickle"
-glob_file = "Data/glob_3.pickle"
+glob_file = "Data/glob_4.pickle"
 vocab_file = "Data/vocab.pickle"
 use_balance = True
-output_format = "fouraxis"
+output_format = "leftright"
 random.seed()
 
 
@@ -27,8 +27,8 @@ def dict_load():
 
 
 def unsparse_raw(sparse_labels):
-    split = 0
-    base_float = 0
+    inclusion_smoothing = 0.5
+    base_float = 0.0
     unsparsed = []
 
     for i in range(len(sparse_labels)):
@@ -36,26 +36,26 @@ def unsparse_raw(sparse_labels):
         base = [base_float] * 9
         if label > 4:
             if label < 7:
-                base[1] = split
+                base[1] = inclusion_smoothing
             if label >= 7:
-                base[0] = split
+                base[0] = inclusion_smoothing
             if label == 5 or label == 8:
-                base[3] = split
+                base[3] = inclusion_smoothing
             else:
-                base[2] = split
-            base[label] = 1 - (split * 2) - (base_float * 6)
+                base[2] = inclusion_smoothing
+            base[label] = 1
         elif label != 4:
             if label == 0 or label == 2:
-                base[7] = split
+                base[7] = inclusion_smoothing
             if label == 1 or label == 3:
-                base[5] = split
+                base[5] = inclusion_smoothing
             if label == 0 or label == 3:
-                base[8] = split
+                base[8] = inclusion_smoothing
             if label == 1 or label == 2:
-                base[6] = split
-            base[label] = 1 - (split * 2) - (base_float * 6)
+                base[6] = inclusion_smoothing
+            base[label] = 1
         else:
-            base[label] = 1 - base_float * 8
+            base[label] = 1
         unsparsed.append(base)
     return unsparsed
 
@@ -73,6 +73,7 @@ def unsparse_four_axis(sparse_labels):
         else:
             new_label.append(0)
         unsparsed_labels.append(new_label)
+
     return unsparsed_labels
 
 
@@ -81,16 +82,16 @@ def write_file(file, obj):
         pickle.dump(obj, write_fp)
 
 
-def create_seq(dropout=0.0, lr_sched=0.0001, dense_size=20, num_layers=1, output_size=1):
+def create_seq(dropout=0.0, lr_sched=0.0001, dense_size=20, num_layers=1, output_size=1, vocab_s=5000):
     dense_layers = []
 
     for i in range(num_layers):
         if dropout > 0:
             dense_layers.append(layers.Dropout(dropout))
-        dense_layers.append(layers.Dense(dense_size, activation='elu',))
+        dense_layers.append(layers.Dense(dense_size, activation='relu',))
 
     model_to_return = keras.models.Sequential()
-    model_to_return.add(tf.keras.Input(shape=(5001,)))
+    model_to_return.add(tf.keras.Input(shape=(vocab_s+1,)))
 
     for layer in dense_layers:
         model_to_return.add(layer)
@@ -112,35 +113,38 @@ def create_seq(dropout=0.0, lr_sched=0.0001, dense_size=20, num_layers=1, output
     return model_to_return
 
 
-def preprocess_tensors(vect_tensors, score_tensors, labels):
+def preprocess_tensors(vect_tensors, score_tensors):
+
+    sum_tensor = np.asarray([i if i != 0 else 1 for i in vect_tensors.sum(axis=1)])
+    vect_tensors = vect_tensors / sum_tensor[:, None] * 100
+    score_tensors = np.asarray([[a] for a in score_tensors])
+    finished_tensors = np.append(vect_tensors, score_tensors, axis=1)
+
+    return finished_tensors
+
+
+def unsparse(labels):
     labels = np.asarray(labels)
     if max(labels) == 3:
         unsparsed = np.asarray(unsparse_four_axis(labels))
     elif max(labels) > 3:
         unsparsed = np.asarray(unsparse_raw(labels))
     else:
-        unsparsed = None
+        unsparsed = labels
 
-    sum_tensor = np.asarray([i if i != 0 else 1 for i in vect_tensors.sum(axis=1)])
-    vect_tensors = vect_tensors / sum_tensor[:, None] * 100
-    score_tensors = np.asarray([[a] for a in score_tensors])
-    prepped_tensors = np.append(vect_tensors, score_tensors, axis=1)
-
-    dict_to_return = {'prepped_tensors': prepped_tensors, 'prepped_labels': unsparsed, 'labels': labels}
-
-    return dict_to_return
+    return unsparsed
 
 
-def create_vectorize_layer(text, max_tokens, vocab_s):
+def init_vectorize_layer(text, max_tokens, vocab_s):
     init_t = time.time()
     if exists(vocab_file):
         with open(vocab_file, 'rb') as load_fp:
             vocab = pickle.load(load_fp)
-        vect_layer = TextVectorization(output_mode='count')
+        vect_layer = TextVectorization(output_mode='count', ngrams=3)
         vect_layer.set_vocabulary(vocab[:vocab_s])
         print("Loaded cached vocabulary in {} seconds".format(delta_time(init_t)))
     else:
-        vect_layer = TextVectorization(max_tokens=max_tokens, output_mode='count')
+        vect_layer = TextVectorization(max_tokens=max_tokens, output_mode='count', ngrams=3)
         vect_layer.adapt(text[:15000])
         with open(vocab_file, 'wb') as write_fp:
             pickle.dump(vect_layer.get_vocabulary(), write_fp)
@@ -150,11 +154,8 @@ def create_vectorize_layer(text, max_tokens, vocab_s):
 
 
 def create_validation_data(split, tensor_dict):
-    if tensor_dict['prepped_labels'] is None:
-        labels = tensor_dict['labels']
-    else:
-        labels = tensor_dict['prepped_labels']
-    prepped = tensor_dict['prepped_tensors']
+    labels = tensor_dict['labels']
+    prepped = tensor_dict['tensors']
     total = len(prepped)
     train_size = round(total * (1-split))
     val_size = total - train_size
@@ -181,12 +182,10 @@ def delta_time(t):
 
 
 def raw_to_left_right(data_dict):
-    text = data_dict['text']
+    tensors = data_dict['tensors']
     labels = data_dict['labels']
-    scores = data_dict['scores']
-    new_text = []
+    new_tensors = []
     new_labels = []
-    new_scores = []
     for index, label in enumerate(labels):
         new_label = None
         if label in [0, 7, 8]:
@@ -195,103 +194,111 @@ def raw_to_left_right(data_dict):
             new_label = 1
         if new_label is not None:
             new_labels.append(new_label)
-            new_text.append(text[index])
-            new_scores.append(scores[index])
-    return {'text': new_text, 'labels': new_labels, 'scores': new_scores}
+            new_tensors.append(tensors[index])
+    return {'labels': new_labels, 'tensors': new_tensors}
 
 
 def raw_to_four_axis(data_dict):
-    text = data_dict['text']
+    tensors = data_dict['tensors']
     labels = data_dict['labels']
-    scores = data_dict['scores']
-    new_text = []
+    new_tensors = []
     new_labels = []
-    new_scores = []
     for index, label in enumerate(labels):
         if label in [5, 6, 7, 8]:
             new_labels.append(label - 5)
-            new_text.append(text[index])
-            new_scores.append(scores[index])
-    return {'text': new_text, 'labels': new_labels, 'scores': new_scores}
+            new_tensors.append(tensors[index])
+    return {'labels': new_labels, 'tensors': new_tensors}
+
+
+def print_dist(labels):
+    counts = dict()
+    for label in labels:
+        if str(label) in counts.keys():
+            counts[str(label)] += 1
+        else:
+            counts[str(label)] = 1
+    print(list(counts.values()))
 
 
 def balance_dict(data_dict):
     labels = data_dict['labels']
-    text = data_dict['text']
-    scores = data_dict['scores']
-    counts = [0] * (max(labels) + 1)
+    tensors = data_dict['tensors']
+    counts = dict()
     new_labels = []
-    new_text = []
-    new_scores = []
+    new_tensors = []
     for label in labels:
-        counts[label] += 1
-    print(counts)
-    min_c = min(counts)
-    for i in range(len(counts)):
+        label_key = str(label)
+        if label_key in counts.keys():
+            counts[label_key] += 1
+        else:
+            counts[label_key] = 1
+    min_c = min(counts.values())
+    for i in counts.keys():
         counts[i] = 0
     for i, label in enumerate(labels):
-        if counts[label] < min_c:
+        label_key = str(label)
+        if counts[label_key] < min_c:
             new_labels.append(label)
-            new_text.append(text[i])
-            new_scores.append(scores[i])
-            counts[label] += 1
-    print(counts)
-    return {'text': new_text, 'labels': new_labels, 'scores': new_scores}
+            new_tensors.append(tensors[i])
+            counts[label_key] += 1
+    return {'tensors': new_tensors, 'labels': new_labels}
 
 
 if __name__ == '__main__':
     init_time = time.time()
     raw_dict = dict_load()
     print("Loaded data in {} seconds.".format(delta_time(init_time)))
-    init_time = time.time()
-    left_right_dict = raw_to_left_right(raw_dict)
-    four_axis_dict = raw_to_four_axis(raw_dict)
 
     loss = "categorical"
     monitor = "val_categorical_crossentropy"
 
-    if output_format == "leftright":
-        loss = "binary"
-        monitor = "val_binary_crossentropy"
-        active_dict = left_right_dict
-        final_layer_size = 1
-    elif output_format == "fouraxis":
-        active_dict = four_axis_dict
-        final_layer_size = 2
-    else:
-        active_dict = raw_dict
-        final_layer_size = 9
-
-    if use_balance:
-        active_dict = balance_dict(active_dict)
-
-    vocab_size = 5000
-    vectorize_layer = create_vectorize_layer(active_dict['text'], 20000, vocab_size)
+    vocab_size = 7500
+    vectorize_layer = init_vectorize_layer(raw_dict['text'], 20000, vocab_size)
 
     init_time = time.time()
     if exists(cached_dict_file):
         with open(cached_dict_file, 'rb') as dict_fp:
-            prepped_dict = pickle.load(dict_fp)
-        print("Loaded cached tensors in {} seconds.".format(delta_time(init_time)))
+            cache_dict = pickle.load(dict_fp)
     else:
-        vectorized_tensors = np.asarray(tf.cast(vectorize_layer(active_dict['text']), tf.float64))
-        prepped_dict = preprocess_tensors(vectorized_tensors, active_dict['scores'], active_dict['labels'])
+        vectorized_tensors = np.asarray(tf.cast(vectorize_layer(raw_dict['text']), tf.float64))
+        prepped_tensors = preprocess_tensors(vectorized_tensors, raw_dict['scores'])
+        cache_dict = {'tensors': prepped_tensors, 'labels': raw_dict['labels']}
         with open(cached_dict_file, 'wb') as dict_fp:
-            pickle.dump(prepped_dict, dict_fp)
-        print("Vectorized and prepped tensors in {} seconds.".format(delta_time(init_time)))
+            pickle.dump(cache_dict, dict_fp)
+    print("Loaded cached tensors in {} seconds.".format(delta_time(init_time)))
     # if you change anything before this point, the cached_tensor.pickle file must be deleted for changes to take effect
     # this incurs a large delay to vectorize the tensors again (1-5min)
-    print(type(prepped_dict))
 
     init_time = time.time()
-    train_tensors, val_tensors, train_labels, val_labels = create_validation_data(0.2, prepped_dict)
-    print("Tensors split in {} seconds.".format(delta_time(init_time)))
+    dicts = {'leftright': raw_to_left_right(cache_dict),
+             'fouraxis': raw_to_four_axis(cache_dict),
+             'raw': cache_dict}
+    for key in dicts.keys():
+        dicts[key]['labels'] = unsparse(dicts[key]['labels'])
+
+    if output_format == "leftright":
+        loss = "binary"
+        monitor = "val_binary_crossentropy"
+        final_layer_size = 1
+    elif output_format == "fouraxis":
+        final_layer_size = 2
+    else:
+        final_layer_size = 9
+
+    active_dict = dicts[output_format]
+
+    if use_balance:
+        active_dict = balance_dict(active_dict)
+
+    print_dist(active_dict['labels'])
+    train_tensors, val_tensors, train_labels, val_labels = create_validation_data(0.2, active_dict)
+    print(f"Tensors prepared in {delta_time(init_time)} seconds.")
     init_time = time.time()
     if len(train_tensors) != len(train_labels) or len(val_labels) != len(val_tensors):
         print("Tensor size does not match label size. Exiting.")
         exit()
-    print("{} training points.".format(len(train_labels)))
-    print("{} validation points.".format(len(val_labels)))
+    print(f"{len(train_labels)} training points.")
+    print(f"{len(val_labels)} validation points.")
 
     old_model = None
     compare_models = False
@@ -307,17 +314,18 @@ if __name__ == '__main__':
         decay_rate=.75,
         staircase=False)
 
-    model = create_seq(lr_sched=lr_schedule, dense_size=1000, dropout=.35, output_size=final_layer_size)
+    model = create_seq(lr_sched=lr_schedule, dense_size=1000, dropout=.35,
+                       output_size=final_layer_size, vocab_s=vocab_size)
 
     print("Model created in {} seconds".format(delta_time(init_time)))
 
     history = model.fit(x=train_tensors,
                         y=train_labels,
-                        epochs=500,
+                        epochs=1000,
                         steps_per_epoch=steps_per_epoch,
                         validation_data=(val_tensors, val_labels),
                         callbacks=tf.keras.callbacks.EarlyStopping(monitor=monitor,
-                                                                   patience=100))
+                                                                   patience=150))
 
     new_metrics = model.evaluate(val_tensors, val_labels)
     if old_model:
